b7ebf27ab1df208b80eb1ba4161210349f6d9e7d
M3MONs <alan.skrrrz@gmail.com>
2024-05-18 18:19:20 +0200

    feat: Add SportsLeftMenu component to HomePage

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -1,9 +1,23 @@
 import Navbar from "src/components/organisms/Navbar/Navbar";
+import SportsLeftMenu from "../organisms/SportsLeftMenu/SportsLeftMenu";
+import styled from "styled-components";
+
+const Main = styled.main`
+    max-width: 1440px;
+    min-height: 100vh;
+    margin: 0 auto;
+    padding: 70px 0;
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+`;
 
 const HomePage = () => {
     return (
         <>
             <Navbar />
+            <Main>
+                <SportsLeftMenu />
+            </Main>
         </>
     );
 };
