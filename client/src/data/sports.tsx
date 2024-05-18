d2935b802dd3972963fa8f7f63d1eedf8f87ee35
M3MONs <alan.skrrrz@gmail.com>
2024-05-18 18:18:38 +0200

    feat: Add sports data and icons

---
+++ client/src/data/sports.tsx
@@ -0,0 +1,10 @@
+import { EU, GB, ES, DE, IT, PL } from "country-flag-icons/react/3x2";
+
+export const sports = [
+    { name: "Europe's best leagues", icon: <EU /> },
+    { name: "Premier League", icon: <GB /> },
+    { name: "La Liga", icon: <ES /> },
+    { name: "Bundesliga", icon: <DE /> },
+    { name: "Serie A", icon: <IT /> },
+    { name: "Ekstraklasa", icon: <PL /> },
+];
