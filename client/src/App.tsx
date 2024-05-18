25e76d51a7ce55675a9b27614b0ac8c234c63777
M3MONs <alan.skrrrz@gmail.com>
2024-05-18 14:46:55 +0200

    feat: Add routing to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "src/components/pages/HomePage";
+
 const App = () => {
-    return <div className='App'></div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
