95229b40a6be3d16573e64c3496458d8af02940c
M3MONs <alan.skrrrz@gmail.com>
2024-05-20 19:45:12 +0200

    feat: Add SportsCompetitions component and styles

---
+++ client/src/components/organisms/SportsCompetitions/SportsCompetitions.style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SportsCompetitionsWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    padding: 0 1.5rem;
+`;
