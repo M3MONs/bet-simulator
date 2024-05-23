f162598d8ad0cc11f9b507e0abd660ca870cd117
M3MONs <alan.skrrrz@gmail.com>
2024-05-23 20:25:45 +0200

    feat: Add EmptyBettingSlip component and styles

---
+++ client/src/components/molecules/EmptyBettingSlip/EmptyBettingSlip.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const EmptyBettingSlipWrapper = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
