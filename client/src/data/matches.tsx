0bf720b81633598eb95e9a70aeefc98e16372af9
M3MONs <alan.skrrrz@gmail.com>
2024-05-23 19:13:24 +0200

    feat: Add event and teams properties to matches in matches.tsx

--- client/src/data/matches.tsx
+++ client/src/data/matches.tsx
@@ -2,8 +2,9 @@ import { IT, PL } from "country-flag-icons/react/3x2";
 
 export const matches = [
     {
+        event: 1,
         head: "Serie A • Round 38",
-        match: "AC Milan vs Salernitana",
+        teams: "AC Milan vs Salernitana",
         flag: <IT />,
         odds: [
             { odd: 1.25, result: "Ac Milan" },
@@ -12,8 +13,9 @@ export const matches = [
         ],
     },
     {
+        event: 2,
         head: "Ekstraklasa • Round 38",
-        match: "Zagłębie Lubin vs ŁKS Łódź",
+        teams: "Zagłębie Lubin vs ŁKS Łódź",
         flag: <PL />,
         odds: [
             { odd: 1.75, result: "Zagłębie Lubin" },
