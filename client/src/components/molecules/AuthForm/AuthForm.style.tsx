2f8e82ec14c92d7981c788b6ce58f0592018f186
M3MONs <alan.skrrrz@gmail.com>
2024-05-26 19:28:11 +0200

    feat: Add AuthForm component and styles

---
+++ client/src/components/molecules/AuthForm/AuthForm.style.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const AuthFormContainer = styled.div`
+    width: 100%;
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 2rem 1.5rem;
+    background: white;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+`;
+
+export const AuthFormTitle = styled.h2`
+    padding: 1rem 0;
+`;
