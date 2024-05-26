ef8049e768b1abe4e2a0500a7996fa94ee3b50d8
M3MONs <alan.skrrrz@gmail.com>
2024-05-26 19:43:21 +0200

    feat: Improve login page functionality and styling

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -3,15 +3,35 @@ import AuthInput from "../atoms/AuthInput";
 import AuthButton from "../atoms/AuthButton";
 import AuthContent from "../organisms/AuthContent/AuthContent";
 import AuthPage from "../templates/AuthPage";
+import { useCallback, useState } from "react";
 
 const LoginPage = () => {
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLoginChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setLogin(e.target.value);
+    }, []);
+
+    const handlePasswordChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }, []);
+
+    const handleLogin = useCallback(() => {
+        // TODO: Implement login logic
+    }, [login, password]);
+
     return (
         <AuthPage>
             <AuthContent type='Login'>
                 <AuthForm title='Login'>
-                    <AuthInput placeholder='Login' type='text' />
-                    <AuthInput placeholder='Password' type='password' />
-                    <AuthButton name='Login' />
+                    <AuthInput placeholder='Login' type='text' handleChange={handleLoginChange} />
+                    <AuthInput
+                        placeholder='Password'
+                        type='password'
+                        handleChange={handlePasswordChange}
+                    />
+                    <AuthButton name='Login' handleClick={handleLogin} />
                     <AuthButton name='Forgot password?' background='white' color='#d2161e' />
                 </AuthForm>
             </AuthContent>
