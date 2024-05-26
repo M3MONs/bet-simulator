ea747c83fdfe5dccfe6587ef2b63f6c863fceb5e
M3MONs <alan.skrrrz@gmail.com>
2024-05-26 18:43:10 +0200

    feat: Add login page to the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "src/components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<HomePage />} />
+                <Route path='/login' element={<LoginPage />} />
             </Routes>
         </Router>
     );
