d41080ccd5c9b6932e1c7a7f41c23d1e1db9763b
M3MONs <alan.skrrrz@gmail.com>
2024-05-28 19:36:21 +0200

    refactor: Update LoginPage component to use AuthContext for user authentication

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,32 +1,45 @@
+import { useCallback, useState } from "react";
+import { useAuth } from "src/context/AuthContext";
 import AuthForm from "../molecules/AuthForm/AuthForm";
 import AuthInput from "../atoms/AuthInput";
-import AuthContent from "../organisms/AuthContent/AuthContent";
 import AuthPage from "../templates/AuthPage";
-import { useCallback, useState } from "react";
 import LinkButton from "../atoms/LinkButton";
 import NormalButton from "../atoms/NormalButton";
+import AuthContent from "../organisms/AuthContent/AuthContent";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-    const [login, setLogin] = useState("");
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const { login } = useAuth();
 
-    const handleLoginChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        setLogin(e.target.value);
+    const handleUsernameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
     }, []);
 
     const handlePasswordChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(e.target.value);
     }, []);
 
-    const handleLogin = useCallback(() => {
-        // TODO: Implement login logic
-    }, [login, password]);
+    const handleLogin = useCallback(async () => {
+        try {
+            await login(username, password);
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }, [username, password]);
 
     return (
         <AuthPage>
             <AuthContent type='Login'>
                 <AuthForm title='Login'>
-                    <AuthInput placeholder='Login' type='text' handleChange={handleLoginChange} />
+                    <AuthInput
+                        placeholder='Login'
+                        type='text'
+                        handleChange={handleUsernameChange}
+                    />
                     <AuthInput
                         placeholder='Password'
                         type='password'
