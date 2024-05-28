08640a43a2cd9f0a47e09ca5b600824b299f0be2
M3MONs <alan.skrrrz@gmail.com>
2024-05-28 19:36:13 +0200

    chore: Wrap routes with AuthProvider in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "src/components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
     return (
         <Router>
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route path='/register' element={<RegisterPage />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/register' element={<RegisterPage />} />
+                </Routes>
+            </AuthProvider>
         </Router>
     );
 };
