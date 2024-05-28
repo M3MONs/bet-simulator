63b255daf0e1c833044edae81d55c1c4b4b49847
M3MONs <alan.skrrrz@gmail.com>
2024-05-28 19:35:50 +0200

    chore: Add API module for making HTTP requests

---
+++ client/src/api/api.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8000/";
+const ACCESS_TOKEN = "access";
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
