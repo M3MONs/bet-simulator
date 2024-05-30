ba022f74a0e9c5e5271cd0a2781eb42d50ba3623
M3MONs <alan.skrrrz@gmail.com>
2024-05-30 13:22:27 +0200

    feat: Add user profile button to NavbarButtons component

--- client/src/components/molecules/NavbarButtons/NavbarButtons.tsx
+++ client/src/components/molecules/NavbarButtons/NavbarButtons.tsx
@@ -2,16 +2,21 @@ import LinkButton from "src/components/atoms/LinkButton";
 import { NavbarButtonsContainer } from "./NavbarButtons.style";
 import { useAuth } from "src/context/AuthContext";
 import NormalButton from "src/components/atoms/NormalButton";
+import { FaUserCircle } from "react-icons/fa";
 
 const NavbarButtons = () => {
     const { accessToken, logout } = useAuth();
     return (
         <NavbarButtonsContainer>
-            // TODO: User profile button
             {accessToken ? (
-                <NormalButton sx={{ background: "white" }} handleClick={logout}>
-                    Logout
-                </NormalButton>
+                <>
+                    <LinkButton to='/profile'>
+                        <FaUserCircle size={22} />
+                    </LinkButton>
+                    <NormalButton sx={{ background: "white" }} handleClick={logout}>
+                        Logout
+                    </NormalButton>
+                </>
             ) : (
                 <>
                     <LinkButton to='/login' sx={{ background: "white" }}>
