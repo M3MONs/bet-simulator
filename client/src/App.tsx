da12216cb6e37bf0f0500826c5ed16dfd5c4f31d
M3MONs <alan.skrrrz@gmail.com>
2024-05-30 12:05:58 +0200

    feat: Add Page404 component for handling 404 errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,18 @@ import HomePage from "src/components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import { AuthProvider } from "./context/AuthContext";
+import Page404 from "./components/pages/Page404";
 
 const App = () => {
     return (
         <Router>
             <AuthProvider>
                 <Routes>
-                    {/* TODO: Add protected routes; Add profile page; Add 404 page; Add coupons page */}
+                    {/* TODO: Add protected routes; Add profile page; Add coupons page */}
                     <Route path='/' element={<HomePage />} />
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
+                    <Route path='*' element={<Page404 />} />
                 </Routes>
             </AuthProvider>
         </Router>
