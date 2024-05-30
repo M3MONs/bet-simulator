f3d37efcf982d2c1905f71b5d3fb161467e4d12e
M3MONs <alan.skrrrz@gmail.com>
2024-05-30 12:46:41 +0200

    feat: Add ProfilePage component for user profiles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import { AuthProvider } from "./context/AuthContext";
 import Page404 from "./components/pages/Page404";
+import ProtectedRoute from "./components/routes/ProtectedRoute";
+import ProfilePage from "./components/pages/ProfilePage";
 
 const App = () => {
     return (
@@ -14,6 +16,14 @@ const App = () => {
                     <Route path='/' element={<HomePage />} />
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
+                    <Route
+                        path='/profile'
+                        element={
+                            <ProtectedRoute>
+                                <ProfilePage />
+                            </ProtectedRoute>
+                        }
+                    />
                     <Route path='*' element={<Page404 />} />
                 </Routes>
             </AuthProvider>
