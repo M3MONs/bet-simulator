1861c764705d223ae72249b558cf9ba289e05d6c
M3MONs <alan.skrrrz@gmail.com>
2024-05-30 12:05:18 +0200

    refactor: Add Page404 component for handling 404 errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "src/components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import { AuthProvider } from "./context/AuthContext";
+import Page404 from "./components/pages/Page404";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
                     <Route path='/' element={<HomePage />} />
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
+                    <Route path='*' element={<Page404 />} />
                 </Routes>
             </AuthProvider>
         </Router>
