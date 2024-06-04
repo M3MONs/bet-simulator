ad1025ded5d51b3cd3ae6eeaa3e849a514d15a23
M3MONs <alan.skrrrz@gmail.com>
2024-06-04 20:26:50 +0200

    feat: Update ToastContainer in index.tsx for better user experience

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
-import { ToastContainer } from "react-toastify";
+import { Slide, ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
         <App />
-        <ToastContainer autoClose={2000} />
+        <ToastContainer position="top-center" autoClose={1000} pauseOnHover={false} transition={Slide} />
     </React.StrictMode>
 );
