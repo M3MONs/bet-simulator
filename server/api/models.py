e69ee9352383072df68d21e438fa09267163db42
M3MONs <alan.skrrrz@gmail.com>
2024-06-07 19:22:49 +0200

    feat: Add Team model to API

--- server/api/models.py
+++ server/api/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create models: Match, Team, Bet, Result, Odd
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    founded = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    external_id = models.CharField(max_length=100, null=True, blank=True)
+    source = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
